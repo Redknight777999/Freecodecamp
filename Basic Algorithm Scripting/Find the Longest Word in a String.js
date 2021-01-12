fd7c46b0ffebca927a268d74eff033b1b5d91e9b
Redknight777999 <65612144+Redknight777999@users.noreply.github.com>
2021-01-12 14:59:49 +0300

    .

--- Basic Algorithm Scripting/Find the Longest Word in a String.js
+++ Basic Algorithm Scripting/Find the Longest Word in a String.js
@@ -16,6 +16,25 @@ function findLongestWordLength(str) {
 	return maxWord;
 }
 
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
+
+//  VARIANT 1 with word number in array to return
+
+function findLongestWordLength(str) {
+	let max = -Infinity;
+	let maxWord = '';
+
+	const arr = str.split(' ');
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i].length > max) {
+			max = arr[i].length
+		}
+
+	}
+	return max;
+}
+
 
 
 
